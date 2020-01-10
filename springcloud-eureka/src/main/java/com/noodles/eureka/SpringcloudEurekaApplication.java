2d0a17c5ba63cb0326179cfb9c71bb4ffe02c89f
Lid23 <645465550@qq.com>
2020-01-10 17:28:23 +0800

    Initial commit

---
+++ springcloud-eureka/src/main/java/com/noodles/eureka/SpringcloudEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.noodles.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableApolloConfig
+public class SpringcloudEurekaApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaApplication.class, args);
+	}
+
+}
---
+++ springcloud-eureka/src/main/java/com/noodles/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.noodles.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().ignoringAntMatchers("/eureka/**");
+		super.configure(http);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/actuator/health");
+	}
+}
