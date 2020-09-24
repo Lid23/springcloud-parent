05dca9c236d83d626007ac8907c3922c381ae3bf
Lid23 <645465550@qq.com>
2020-09-24 16:25:02 +0800

    Springcloud eureka， gateway， feign整理

--- springcloud-test-parent/springcloud-test-client/src/main/java/com/noodles/springcloudtestclient/config/EarlyInitFeignClientOnContextRefresh.java
+++ springcloud-test-parent/springcloud-test-client/src/main/java/com/noodles/springcloudtestclient/config/EarlyInitFeignClientOnContextRefresh.java
@@ -4,8 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
